2e90b03aa8e70b2c917fd960efeb1f510ecf36b5
Oscar S <oscar_sanchez_silva@hotmail.com>
2020-05-30 17:05:40 -0500

    PAGINA HIKING

---
+++ hiking/index.php
@@ -0,0 +1,137 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="/css/all.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;800&display=swap">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+        <title>Hiking</title>
+    </head>
+    <body>
+     
+        <div class="container-fluid fondo-principal">
+      <div class="row mt-4 justify-content-center">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="col-12 col-md-4">
+            <img src="img/logo.png" alt="" />
+          </div>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="col-12 col-md-9 collapse navbar-collapse"
+            id="navbarNavAltMarkup"
+          >
+            <div class="navbar-nav">
+              <a class="nav-item nav-link" href="#">Home</a>
+              <a class="nav-item nav-link" href="#">Upcoming treks</a>
+              <a class="nav-item nav-link" href="#">Trek for family</a>
+              <a class="nav-item nav-link" href="#">Contact us</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+                <div class="row">
+                    <div class="col-12 col-md-2"></div>
+                    <div class="col-12 col-md-6 texto-header">
+                        <h2>CLIMB FOR BETTER</h2>
+                        <h1>GET OUT NOW</h1>
+                        <p>JOIN US FOR THE WEEKEND TO CLIMB</p>
+                        <P>THE HIGHEST PICK IN ENGLAND</P>
+                        <a class="boton-negro" href="#">VIEW UPCOMING TREKS</a>
+                    </div>
+                </div>
+            </div>
+        <div class="continer-fuid">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="h-gris">CHOOSE</h1>
+                    <h1>These best</h1>
+                    <h1>Treks for Spring</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ut maxime quidem nemo? Sequi expedita iste cumque fugit quam, pariatur nemo error aperiam ut accusantium nobis alias tempora. Ipsa, quam!
+                </div>
+                <div class="col-12 col-md-3">
+                    <img class="img-fluid" src="img/foto1.png" alt="">
+                </div>
+                <div class="col-12 col-md-3">
+                    <img class="img-fluid" src="img/foto2.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid mt-4 fondo-bosque">
+            <div class="row justify-content-center">
+                <div class="col-8 text-center justify-content-center">
+                   <h1>WE'RE THE LARGEST & SAFEST</h1>
+                   <h1>HIKING ORGANISATION</h1> 
+                   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo nulla architecto corrupti quos dolor! Ab, neque vitae deserunt nihil non natus! Doloribus provident accusantium reiciendis explicabo minus error numquam. Perspiciatis!</p>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid mt-5">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-4">
+                    <img src="img/imagen1.png" alt="">
+                </div>
+                <div class="col-12 col-md-4 py-4">
+                    <h1>Want To Stay In Touch</h1>
+                    <h1>With The Hiking World?</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni debitis nisi dicta minima explicabo earum eaque, non quia, doloribus quo ut porro? Incidunt corrupti tempora unde sapiente natus nihil doloremque.</p>
+                    <a class="boton-negro" href="#">YES KEEP ME POSTED!</a>
+                </div>
+            </div>
+        </div>
+        <div class="container fondo-gris py-3">
+            <div class="row mt-4 justify-content-center">
+                <div class="col-12 col-md-3">
+                    <img src="img/logo.png" alt=""/>
+                </div>
+                <div class="col-12 col-md-9">
+                    <ul class="menu">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Upcoming trecks</a></li>
+                        <li><a href="#">Trek for family</a></li>
+                        <li><a href="#">Contact us</a></li>
+                    </ul>
+                </div>
+        </div>
+        <div class="row mt-3 justify-content-center">
+            <div class="col-12 col-md-4">
+                <h2 class="h-footer">CALL NOW</h2>
+                <br>
+                <h2 class="h-footer">1800 888 555</h2>
+            </div>
+            <div class="col-12 col-md-4">
+                &copy;Clase Front-End 2020. All Rights Reserved.<br>
+                <a href="http://www.freepik.com">Designed by Freepik.</a>
+
+            </div>
+            <div class="col-12 col-md-4">
+                <a class="icono-social" href="#" alt="Instagram"><i class="fab fa-instagram"></i></a>
+                <a class="icono-social" href="#" alt="Facebook"><i class="fab fa-facebook-f"></i></a>
+                <a class="icono-social" href="#" alt="Twitter"><i class="fab fa-twitter"></i></a>
+                <a class="icono-social" href="#" alt="Whatsapp"><i class="fab fa-whatsapp"></i></a>
+            </div>
+        </div>
+        
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+                
+    </body>
+</html>
